36d0520729b7c791d878a0b6ce1da4abf904b48e
mohamed kamal <mkahf5103@gmail.com>
2022-06-30 19:13:37 +0400

    add geners controller

---
+++ MoviesApi/Controllers/GenresController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesApi.Data;
+
+namespace MoviesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenresController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllasync()
+        {
+            var genres = await _context.genres.ToListAsync();
+            return Ok(genres);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Createasync(CreateGenredto dto)
+        {
+            var grnre = new genre { Name = dto.Name };
+            await _context.genres.AddAsync(grnre);
+            _context.SaveChanges();
+            return Ok(grnre);
+        }
+    }
+}
--- MoviesApi/GlobalUsings.cs
+++ MoviesApi/GlobalUsings.cs
@@ -1,2 +1,4 @@
 ﻿global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
+global using MoviesApi.Dtos;
+ global using MoviesApi.Models;
