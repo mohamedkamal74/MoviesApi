ca8c4a946afc1c6ef9eb649090534db211867c9a
mohamed kamal <mkahf5103@gmail.com>
2022-07-01 19:44:28 +0400

    add edit movie

--- MoviesApi/Controllers/MoviesController.cs
+++ MoviesApi/Controllers/MoviesController.cs
@@ -33,9 +33,19 @@ namespace MoviesApi.Controllers
                 return NotFound($"No Movie with this Id: {id}");
             return Ok(movie);
         }
+
+        [HttpGet("GetByGenreId")]
+        public async Task<IActionResult> GetByGenreIdAsync(byte genreId)
+        {
+            var movies = await _context.Movies.Where(m=>m.GenreId==genreId).Include(g => g.genre).OrderByDescending(m => m.Rate).ToListAsync();
+            return Ok(movies);
+        }
+
         [HttpPost]
         public async Task<IActionResult> CreateAsync([FromForm] MovieDto dto)
         {
+            if(dto.Poster ==null)
+                return BadRequest("Pster is required");
             if (!_allowedExtention.Contains(Path.GetExtension(dto.Poster.FileName).ToLower()))
                 return BadRequest("only .jpg,.png movies are alloewd !");
 
@@ -62,6 +72,52 @@ namespace MoviesApi.Controllers
             _context.SaveChanges();
             return Ok(movie);
 
+
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult>UpdateAsync(int id,[FromBody] MovieDto dto)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+                return NotFound($"No movie found with Id: {id}");
+            var isValidGenre = await _context.genres.AnyAsync(g => g.Id == dto.GenreId);
+            if (!isValidGenre)
+                return BadRequest("Invalid Genre Id !");
+
+            if(dto.Poster != null)
+            {
+                if (!_allowedExtention.Contains(Path.GetExtension(dto.Poster.FileName).ToLower()))
+                    return BadRequest("only .jpg,.png movies are alloewd !");
+
+                if (dto.Poster.Length > _maxallowedPosterSize)
+                    return BadRequest("max size for poster image is 1 mega byte !");
+
+                using var datastream = new MemoryStream();
+                await dto.Poster.CopyToAsync(datastream);
+                movie.Poster=datastream.ToArray();
+            }
+            movie.Title = dto.Title;
+            movie.Year = dto.Year;
+            movie.GenreId = dto.GenreId;
+            movie.StoreLine = dto.StoreLine;
+            movie.Rate = dto.Rate;
+
+            _context.SaveChanges();
+            return Ok(movie);
+
+                
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult>DeleteMovie(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+                return NotFound($"No movie found with Id: {id}");
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+            return Ok(movie);
 
         }
     }
--- MoviesApi/Dtos/MovieDto.cs
+++ MoviesApi/Dtos/MovieDto.cs
@@ -8,7 +8,7 @@
         public double Rate { get; set; }
         [MaxLength(2500)]
         public string StoreLine { get; set; }
-        public IFormFile Poster { get; set; }
+        public IFormFile? Poster { get; set; }
         public byte GenreId { get; set; }
     }
 }
