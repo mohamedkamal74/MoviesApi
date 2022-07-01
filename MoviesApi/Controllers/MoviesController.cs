3c145b5add29429a3f5c1c6a1f8723fbddbc672d
mohamed kamal <mkahf5103@gmail.com>
2022-07-01 18:50:56 +0400

    add end points for get all and get by id movies

--- MoviesApi/Controllers/MoviesController.cs
+++ MoviesApi/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MoviesApi.Data;
 
 namespace MoviesApi.Controllers
@@ -9,15 +10,43 @@ namespace MoviesApi.Controllers
     public class MoviesController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private new List<string> _allowedExtention = new List<string> { ".jpg", ".png" };
+        private long _maxallowedPosterSize =1048576;
+
 
         public MoviesController(ApplicationDbContext context)
         {
             _context = context;
         }
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var movies = await _context.Movies.Include(g=>g.genre).OrderByDescending(m=>m.Rate).ToListAsync();
+            return Ok(movies);
+        }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var movie = await _context.Movies.Include(g=>g.genre).SingleOrDefaultAsync(m=>m.Id==id);
+            if (movie == null)
+                return NotFound($"No Movie with this Id: {id}");
+            return Ok(movie);
+        }
         [HttpPost]
         public async Task<IActionResult> CreateAsync([FromForm] MovieDto dto)
         {
+            if (!_allowedExtention.Contains(Path.GetExtension(dto.Poster.FileName).ToLower()))
+                return BadRequest("only .jpg,.png movies are alloewd !");
+
+            if(dto.Poster.Length>_maxallowedPosterSize)
+                return BadRequest("max size for poster image is 1 mega byte !");
+
+            var isValidGenre = await _context.genres.AnyAsync(g => g.Id == dto.GenreId);
+            if(!isValidGenre)
+                return BadRequest("Invalid Genre Id !");
+
+
             using var datastream=new MemoryStream();
             await dto.Poster.CopyToAsync(datastream);
             var movie = new Movie
